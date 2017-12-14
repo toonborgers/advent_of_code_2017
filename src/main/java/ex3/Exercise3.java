7acbe91ba4f44b3642bd7369e1801d4ce82b9f64
Toon Borgers <borgers.toon@gmail.com>
2017-12-14 16:13:45 +0100

    Exercise 3 part 2

--- src/main/java/ex3/Exercise3.java
+++ src/main/java/ex3/Exercise3.java
@@ -1,42 +1,87 @@
 package ex3;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Exercise3 {
     public static void main(String[] args) {
-        for (DIRECTION direction : DIRECTION.values()) {
-            System.out.println(direction + ", " + direction.next());
-        }
+//        part1(312051);
+        part2(312051);
+    }
 
-        Map<Point, Integer> values = new HashMap<>();
+
+    private static void part2(int input) {
+        Map<Point, Integer> pointsToValues = new HashMap<>();
+
         int value = 1;
         DIRECTION direction = DIRECTION.RIGHT;
         int x = 0;
         int y = 0;
         int size = 1;
 
-        values.put(new Point(0, 0), 1);
-        while (value < 9) {
+        pointsToValues.put(new Point(0, 0), 1);
+        while (value <= input) {
+
             if (direction.isHorizontal() && Math.abs(x) == size) {
                 direction = direction.next();
-            }
-
-            if (direction.isVertical() && Math.abs(y) == size) {
-                direction = direction.next();
+            } else if (direction.isVertical() && Math.abs(y) == size) {
                 if (direction == DIRECTION.DOWN) {
                     size++;
                 }
+                direction = direction.next();
             }
 
-            value++;
             x += direction.addX;
             y += direction.addY;
 
-            System.out.println("(" + x + ", " + y + "): " + value);
-            values.put(new Point(x, y), value);
+            Point point = new Point(x, y);
+            value = sumOfSurrounding(point, pointsToValues);
+            pointsToValues.put(point, value);
         }
+
+
+        System.out.println(value);
     }
+
+    private static int sumOfSurrounding(Point point, Map<Point, Integer> pointsToValues) {
+        return point.getSurrounding().stream()
+                .mapToInt(p -> pointsToValues.getOrDefault(p, 0))
+                .sum();
+    }
+
+
+    private static void part1(int valueToFind) {
+        Map<Integer, Point> values = new HashMap<>();
+        int value = 1;
+        DIRECTION direction = DIRECTION.RIGHT;
+        int x = 0;
+        int y = 0;
+        int size = 1;
+
+        values.put(1, new Point(0, 0));
+        while (value < valueToFind) {
+
+            if (direction.isHorizontal() && Math.abs(x) == size) {
+                direction = direction.next();
+            } else if (direction.isVertical() && Math.abs(y) == size) {
+                if (direction == DIRECTION.DOWN) {
+                    size++;
+                }
+                direction = direction.next();
+            }
+
+            x += direction.addX;
+            y += direction.addY;
+
+            values.put(++value, new Point(x, y));
+        }
+
+        Point p = values.get(valueToFind);
+        System.out.println(Math.abs(p.x) + Math.abs(p.y));
+    }
+
 
     enum DIRECTION {
         RIGHT(1, 0), UP(0, -1), LEFT(-1, 0), DOWN(0, 1);
@@ -61,13 +106,27 @@ public class Exercise3 {
         }
     }
 
+
     static class Point {
         int x;
         int y;
 
-        public Point(int x, int y) {
+        Point(int x, int y) {
             this.x = x;
             this.y = y;
+        }
+
+        List<Point> getSurrounding() {
+            return Arrays.asList(
+                    new Point(x + 1, y),
+                    new Point(x + 1, y + 1),
+                    new Point(x, y + 1),
+                    new Point(x - 1, y + 1),
+                    new Point(x - 1, y),
+                    new Point(x - 1, y - 1),
+                    new Point(x, y - 1),
+                    new Point(x + 1, y - 1)
+            );
         }
 
         @Override
@@ -86,6 +145,14 @@ public class Exercise3 {
             int result = x;
             result = 31 * result + y;
             return result;
+        }
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    '}';
         }
     }
 }
